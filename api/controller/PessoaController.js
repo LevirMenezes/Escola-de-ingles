35000ba0bbf399940e1fb798b1f9c2f88c0722e8
Levir Menezes <54421490+LevirMenezes@users.noreply.github.com>
2023-04-20 11:15:39 -0400

    refactore (api-escola-ingles): melhora funcionalizades.
    FORMAÇÃO: ORM com Node: Avançando nas funcionalidades do Sequelize.

--- api/controller/PessoaController.js
+++ api/controller/PessoaController.js
@@ -1,9 +1,24 @@
-const database = require('../models');
+//const database = require('../models');
+// const Sequelize = require('sequelize');
+const { PessoasServices } = require('../services');
+const pessoasServices = new PessoasServices();
+
 
 class PessoaController {
+
+  static async pegaPessoasAtivas(req, res) {
+    try {
+      const PessoasAtivas = await pessoasServices.pegaRegistrosAtivos();
+
+      return res.status(200).json(PessoasAtivas);
+    } catch (erro) {
+      return res.status(500).json(erro.message);
+    }
+  }
+
   static async pegaTodasAsPessoas(req, res) {
     try {
-      const todasAsPessoas = await database.Pessoas.findAll();
+      const todasAsPessoas = await pessoasServices.pegaTodosOsRegistros();
 
       return res.status(200).json(todasAsPessoas);
     } catch (erro) {
@@ -14,7 +29,7 @@ class PessoaController {
   static async pegaUmaPessoa(req, res) {
     try {
       const id = req.params.id;
-      const umaPessoa = await database.Pessoas.findOne({ where: { id: id } });
+      const umaPessoa = await pessoasServices.pegaUmRegistro({ id: Number(id) });
 
       return res.status(200).json(umaPessoa);
     } catch (erro) {
@@ -25,7 +40,7 @@ class PessoaController {
   static async criaPessoa(req, res) {
     const novaPessoa = req.body;
     try {
-      const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+      const novaPessoaCriada = await pessoasServices.criaRegistro(novaPessoa);
       return res.status(200).json(novaPessoaCriada);
 
     } catch (erro) {
@@ -37,8 +52,9 @@ class PessoaController {
     const id = req.params.id;
     const novasInfos = req.body;
     try {
-      await database.Pessoas.update(novasInfos, { where: { id: Number(id) } });
-      const pessoaAtualizada = await database.Pessoas.findOne({ where: { id: id }});
+      await pessoasServices.atualizaRegistro(novasInfos, Number(id));
+      const pessoaAtualizada = await pessoasServices.pegaUmRegistro({ id: id });
+
       return res.status(200).json(pessoaAtualizada);
 
 
@@ -52,7 +68,8 @@ class PessoaController {
 
     try
     {
-      await database.Pessoas.destroy({ where: { id: Number(id) } });
+      await pessoasServices.apagaRegistro(Number(id));
+
       return res.status(200).json({ mensagem: `id ${id} deletado` })
     }
     catch (erro)
@@ -61,79 +78,30 @@ class PessoaController {
     }
   }
 
+  static async restauraPessoa(req, res) {
+    const { id } = req.params;
+    try
+    {
+      await pessoasServices.restauraRegistro(Number(id));
 
-  static async pegaUmaMatricula(req, res) {
-    const { estudanteId, matriculaId } = req.params;
-    try {
-      const umaMatricula = await database.Matriculas.findOne({
-        where: {
-          id: Number(matriculaId),
-          estudante_id: Number(estudanteId)
-        }
-      });
-
-      umaMatricula.estudante_id = await database.Pessoas.findOne({ where: { id: Number(estudanteId) } });
-
-      return res.status(200).json(umaMatricula);
-    } catch (erro) {
-      return res.status(500).json(erro.message);
+      return res.status(200).json({ mensagem: `O id ${id} foi restaurado!`})
+    } catch (erro)
+    {
+      return res.status(500).json(erro.message)
     }
   }
 
-  static async criaMatricula(req, res) {
+  static async cancelaPessoa(req, res) {
     const { estudanteId } = req.params;
-    const novaMatricula = { ...req.body, estudante_id: Number(estudanteId) };
     try
     {
-      const novaMatriculaCriada = await database.Matriculas.create(novaMatricula);
+      await pessoasServices.cancelaPessoaEMatriculas(Number(estudanteId));
 
-      return res.status(200).json(novaMatriculaCriada);
+      return res.status(200).json({ message: `matriculas ref. ${estudanteId} canceladas` });
+
     } catch (erro)
     {
       return res.status(500).json(erro.message);
-    }
-  }
-
-  static async atualizaMatricula(req, res) {
-    const { estudanteId, matriculaId } = req.params;
-    const infoAtualizada = req.body;
-    try
-    {
-      await database.Matriculas.update(infoAtualizada, {
-        where: {
-          id: Number(matriculaId),
-          estudante_id: Number(estudanteId)
-        }
-      });
-      const matriculaAtualizada = await database.Matriculas.findOne({
-        where: {
-          id: Number(matriculaId)
-        }
-      });
-
-      return res.status(200).json(matriculaAtualizada);
-    } catch (erro)
-    {
-      return res.status(500).json(erro.message);
-    }
-  }
-
-  static async apagaMatricula(req, res) {
-    const { estudanteId, matriculaId } = req.params;
-    try
-    {
-      await database.Matriculas.destroy({
-        where:
-        {
-          id: Number(matriculaId),
-          estudante_id: Number(estudanteId)
-        }
-      });
-
-      return res.status(200).json({ mensagem: `O id ${matriculaId} foi deletado!` });
-    } catch (erro)
-    {
-      res.status(500).json(erro.message);
     }
   }
 }
--- api/routes/niveisRoute.js
+++ api/routes/niveisRoute.js
@@ -2,11 +2,19 @@ const { Router } = require('express')
 const NivelController = require('../controller/NivelController.js')
 
 const router = Router()
+
 router
   .get('/niveis', NivelController.pegaTodosOsNiveis)
   .get('/niveis/:id', NivelController.pegaUmNivel)
+
+router
   .post('/niveis', NivelController.criaNivel)
+  .post('/niveis/:id/restaura', NivelController.restauraNivel)
+
+router
   .put('/niveis/:id', NivelController.atualizaNivel)
+
+router
   .delete('/niveis/:id', NivelController.apagaNivel)
 
 module.exports = router
